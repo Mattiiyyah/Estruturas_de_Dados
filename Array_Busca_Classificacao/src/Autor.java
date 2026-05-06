be4d5dce6b3064ea871fc4de8304b81b4463dc1d
Matheus de Paula Santos <matheus1516paula@gmail.com>
2026-05-06 08:12:01 -0300

    Array de busca

---
+++ Array_Busca_Classificacao/src/Autor.java
@@ -0,0 +1,31 @@
+// -------------------------------------------
+// Disc.: IED-001 - ESTRUTURAS DE DADOS
+// Prof.: CARLOS MAGNUS CARLSON FILHO
+// Lab. : BUSCA e CLASSIFICAÇÃO EM ARRAYS JAVA
+// Ano/S: 2026/1S
+// -------------------------------------------
+
+// código da classe
+public class Autor {
+    
+	// Uso geral - exibição de dados do autor do trabalho
+	// --------------------------------------------------
+	public void exibeDadosAutor() {
+	
+		// identificação do autor do trabalho
+		System.out.println();
+		System.out.println("-------------------------------------");
+		System.out.println("Disc.: ESTRUTURAS DE DADOS - 2026/1S");
+		System.out.println("Lab. : ARRAYS - BUSCA E CLASSIFICAÇÃO");
+		System.out.println("-------------------------------------");
+		System.out.println("Nome : MATHEUS DE PAULA SANTOS");
+		System.out.println("R.A. : 121 092 AAS P NNN");
+		System.out.println("-------------------------------------");
+		System.out.println();
+	
+		// finalização (desnecessário neste caso, pois é procedimento 'void')
+		return;	
+	}
+
+}
+
