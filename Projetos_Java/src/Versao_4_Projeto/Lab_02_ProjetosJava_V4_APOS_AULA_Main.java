2f5b65050a6ed92abbd6fe815fecbfc2345696c0
Matheus de Paula Santos <matheus1516paula@gmail.com>
2026-05-06 08:27:51 -0300

    Estruturação de projeto com Java

---
+++ Projetos_Java/src/Versao_4_Projeto/Lab_02_ProjetosJava_V4_APOS_AULA_Main.java
@@ -0,0 +1,31 @@
+package Versao_4_Projeto;
+// ------------------------------------
+// Disc.: IED-001 - ESTRUTURAS DE DADOS
+// Prof.: Carlos - FATEC S J Rio Preto
+// Lab. : PROJETOS JAVA
+// Ano/S: 2026/1S
+// ------------------------------------
+
+// VERSÃO 4 (antes da aula): idem final da versão 3
+// - dois arquivos separados
+// - ações realizadas por métodos separados do 'main'
+// --------------------------------------------------
+
+// importação de classe para leitura de dados do teclado -> NÃO PRECISA MAIS
+//import java.util.Scanner;
+
+// classe que contém o PROGRAMA PRINCIPAL ('main')
+public class Lab_02_ProjetosJava_V4_APOS_AULA_Main {
+
+	// PROGRAMA PRINCIPAL
+	// ------------------
+	public static void main(String[] args) throws Exception {
+
+		//instanciaçao de um obejto da classe '...Cordenador'
+		Lab_02_ProjetosJava_V4_APOS_AULA_Coordenador coord = new Lab_02_ProjetosJava_V4_APOS_AULA_Coordenador();
+		
+		//adicionando o métdo 'de entrada' do objeto
+		coord.executaTudo();
+	}
+
+}
